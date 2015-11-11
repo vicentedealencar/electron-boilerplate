2fe3677be053523174d62beb01ae86bd5dc63d3d
Vicente de Alencar <vicentedealencar@gmail.com>
2015-11-11 14:16:18 -0200

    (wip) releasing autoupdatable

---
+++ boilerplate/Gruntfile.js
@@ -0,0 +1,23 @@
+module.exports = function(grunt) {
+
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		'create-windows-installer': {
+			ia32: {
+				appDirectory: './dist/App-win32-ia32',
+				outputDirectory: './dist/release',
+				authors: 'My App Inc.',
+				exe: '<%= pkg.productName %>.exe',
+				description: '<%= pkg.description %>',
+				version: '<%= pkg.version %>',
+				remoteReleases: ''
+			}
+		},
+	});
+
+	grunt.loadNpmTasks('grunt-electron-installer')
+
+	grunt.registerTask('default', ['create-windows-installer']);
+};
